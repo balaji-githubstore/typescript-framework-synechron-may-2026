bb4098cee31df1217884d94c1d1935c4e22143b6
balaji-githubstore <balajidinakaran1@gmail.com>
2026-05-16 15:24:55 +0530

    Modified description name

--- tests/login-ui.spec.ts
+++ tests/login-ui.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test"
 
-test.describe("OrangeHRM Login", () => {
+test.describe("orangeHRM login UI tests", () => {
 
     test('verify title', async ({ page }) => {
         await page.goto('/');
