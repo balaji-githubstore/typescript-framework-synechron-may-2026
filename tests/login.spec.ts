73aad959316782677610c1b23b2bb54d06c14d93
balaji-githubstore <balajidinakaran1@gmail.com>
2026-05-16 16:31:00 +0530

    Added Valid and Invalid Login Test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,16 +3,16 @@ import { test, expect } from "../fixtures/base-test.js"
 test.describe("orangeHRM login tests", () => {
 
     test('verify valid login', async ({ page }) => {
-       //enter username as Admin
-       //password as admin123
-       //click on login 
-       //verify "Dashboard" header
+        await page.locator("xpath=//input[@name='username']").fill("Admin");
+        await page.locator("xpath=//input[@name='password']").fill("admin123");
+        await page.locator("xpath=//button[normalize-space()='Login']").click();
+        await expect(page.locator("//h6[contains(normalize-space(),'Dash')]")).toHaveText("Dashboard");
     });
 
     test('verify invalid login', async ({ page }) => {
-         //enter username as john
-       //password as john223
-       //click on login 
-       //verify "Invalid credentials" error
+        await page.locator("xpath=//input[@name='username']").fill("john");
+        await page.locator("xpath=//input[@name='password']").fill("admin123");
+        await page.locator("xpath=//button[normalize-space()='Login']").click();
+        await expect(page.locator("//p[contains(normalize-space(),'Invalid')]")).toHaveText("Invalid credentials");
     });
 })
