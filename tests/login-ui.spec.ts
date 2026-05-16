c78fc3aeb1466357622f7295cd45708d7bb48c11
balaji-githubstore <balajidinakaran1@gmail.com>
2026-05-16 15:23:31 +0530

    Project Setup first commit

---
+++ tests/login-ui.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test"
+
+test.describe("OrangeHRM Login", () => {
+
+    test('verify title', async ({ page }) => {
+        await page.goto('/');
+        await expect(page).toHaveTitle("OrangeHRM");
+
+    });
+
+    test('verify header', async ({ page }) => {
+        await page.goto('/');
+        await expect(page.locator("xpath=//h5[text()='Login']")).toHaveText("Login")
+    });
+
+    test('verify username and password placeholder', async ({ page }) => {
+        await page.goto('/');
+        await expect(page.locator("xpath=//input[@name='username']")).toHaveAttribute("placeholder", "Username")
+        await expect(page.locator("xpath=//input[@name='password']")).toHaveAttribute("placeholder", "Password")
+    });
+})
