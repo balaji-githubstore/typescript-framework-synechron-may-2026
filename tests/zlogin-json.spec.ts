a0b9a23079d267f5ec4cd2bc7898829b877a4dba
balaji-githubstore <balajidinakaran1@gmail.com>
2026-05-17 12:09:31 +0530

    Updated json data driven

--- tests/zlogin-json.spec.ts
+++ tests/zlogin-json.spec.ts
@@ -5,17 +5,21 @@ import { JsonUtils } from "../utils/json-utlis.js"
 
 test.describe("orangeHRM login tests", () => {
 
-    test('verify valid login', async ({ page }) => {
-        const loginPage = new LoginPage(page);
-        await loginPage.fillUsername("Admin");
-        await loginPage.fillPassword("admin123");
-        await loginPage.clickOnLogin();
+    //update valid login test method with data driven from json - key - validLoginData
+    const validLoginData = JsonUtils.getJson("validLoginData");
+    for (const { username, password, expectedText } of validLoginData) {
+        test(`verify valid login: ${username} and ${password}`, async ({ page }) => {
+            const loginPage = new LoginPage(page);
+            await loginPage.fillUsername(username);
+            await loginPage.fillPassword(password);
+            await loginPage.clickOnLogin();
 
-        const dashboardPage = new DashboardPage(page);
-        await dashboardPage.validateDashboardHeader("Dashboard");
-    });
+            const dashboardPage = new DashboardPage(page);
+            await dashboardPage.validateDashboardHeader(expectedText);
+        });
+    }
 
-    const invalidLoginData=JsonUtils.getJson("invalidLoginData");
+    const invalidLoginData = JsonUtils.getJson("invalidLoginData");
     for (const { username, password, expectedError } of invalidLoginData) {
         test(`verify invalid login: ${username} and ${password}`, async ({ page }) => {
             const loginPage = new LoginPage(page);
