3c8dee5bc0bc8d271e752c72d212700cc31694b0
balaji-githubstore <balajidinakaran1@gmail.com>
2026-05-17 10:54:03 +0530

    Data Driven Using ts file

---
+++ tests/zlogin-data-driven.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "../fixtures/base-test.js"
+import { DashboardPage } from "../pages/DashboardPage.js";
+import { LoginPage } from "../pages/LoginPage.js";
+import { invalidLoginData } from "../utils/data-source.js"
+
+test.describe("orangeHRM login tests", () => {
+
+    test('verify valid login', async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.fillUsername("Admin");
+        await loginPage.fillPassword("admin123");
+        await loginPage.clickOnLogin();
+
+        const dashboardPage = new DashboardPage(page);
+        await dashboardPage.validateDashboardHeader("Dashboard");
+    });
+
+    for (const { username, password, expectedError } of invalidLoginData) {
+        test(`verify invalid login: ${username} and ${password}`, async ({ page }) => {
+            const loginPage = new LoginPage(page);
+            await loginPage.fillUsername(username);
+            await loginPage.fillPassword(password);
+            await loginPage.clickOnLogin();
+            await loginPage.validateInvalidErrorMessage(expectedError);
+        });
+    }
+
+})
---
+++ utils/data-source.ts
@@ -0,0 +1,4 @@
+export const invalidLoginData=[
+    {username:"john",password:"john123",expectedError:"Invalid credentials"},
+    {username:"saul",password:"saul123",expectedError:"Invalid credentials"}
+]
